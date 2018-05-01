b565cfee1ea43f1118ca7282c897ee13b8beda1a
jakubfiala <my email>
2018-05-01 18:22:57 +0100

    cleaned up examples and dependencies

--- index.jsx
+++ index.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { render } from 'react-dom';
-
 import RAudioContext from './src/base/audio-context.jsx';
 import RPipeline from './src/graph/pipeline.jsx';
 import RSplit from './src/graph/split.jsx';
@@ -53,35 +50,3 @@ export {
   RStereoPanner,
   RWaveShaper
 }
-
-
-/**
-=====
-DEMO CODE
-=====
-**/
-import examples from './examples/index.jsx';
-const example = location.hash.slice(1);
-
-const onExampleChange = e => {
-  location.hash = e.target.value;
-  location.reload();
-};
-
-render(
-  (
-    <main>
-      <header>
-      <label htmlFor="example-select">Select an example: </label>
-      <select id="example-select" onChange={onExampleChange} value={example}>
-      {
-        Object.keys(examples).map((ex, ei) => <option key={ei} value={ex}>{ex}</option>)
-      }
-      </select>
-      </header>
-      <hr/>
-      { examples[example] || null }
-    </main>
-  ),
-  document.getElementById('app')
-);
