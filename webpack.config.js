0bb44c48fc450aacc2fdf664dd7c1313f59e4204
jakubfiala <my email>
2018-05-01 18:37:01 +0100

    made examples/demo work again

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,22 @@ const path = require('path');
 module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'reaudio.min.js',
+        filename: 'r-audio.min.js',
     },
-    entry: './index.jsx',
+    entry: './examples/index.jsx',
     devtool: 'source-map',
     module: {
         loaders: [
             {
                 test: /\.js$|\.jsx$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 loaders: ['babel-loader?presets[]=react,env']
             }
         ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'examples'),
+      compress: true,
+      port: 8080
     }
 };
