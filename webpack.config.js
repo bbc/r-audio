26a5e25b90c9365fab56b1c796185c7147de1e1c
jakubfiala <my email>
2018-06-18 15:57:49 +0100

    fixed indents in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'r-audio.min.js',
-    },
-    entry: './examples/index.jsx',
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                loaders: ['babel-loader?presets[]=react,env']
-            }
-        ]
-    },
-    devServer: {
-      contentBase: path.join(__dirname, 'examples'),
-      compress: true,
-      port: 8080
-    }
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'r-audio.min.js'
+  },
+  entry: './examples/index.jsx',
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader?presets[]=react,env']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'examples'),
+    compress: true,
+    port: 8080
+  }
 };
